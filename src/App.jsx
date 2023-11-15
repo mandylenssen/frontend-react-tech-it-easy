0ab97f25c74b2b7db9894039053b5c65066b0b1c
mandylenssen <mandylenssen@gmail.com>
2023-11-15 23:05:01 +0100

    uitwerkingen deel 2 - afgerond

--- src/App.jsx
+++ src/App.jsx
@@ -13,15 +13,24 @@ import minus from "./assets/minus.png"
 function App() {
 
     function mostSoldFirst() {
-        console.log("Meest verkocht eerst")
+        {inventory.sort((a, b) => {
+            return b.sold - a.sold;
+        })}
+        console.log(inventory);
     }
 
     function cheapestFirst() {
-        console.log("Goedkoopste eerst")
+        {inventory.sort((a,b) => {
+            return a.price - b.price;
+        })}
+        console.log(inventory)
     }
 
     function mostSuitedForSport() {
-        console.log("Meest geschikt voor sport eerst")
+        {inventory.sort((a, b) => {
+            return b.refreshRate - a.refreshRate
+        })}
+        console.log(inventory)
     }
 
     return (
@@ -48,7 +57,7 @@ function App() {
             <section>
                 <h2>Best verkochte tv</h2>
                 <div className="best-product-panel">
-                    <img src={bestSellingTv.sourceImg} alt="tv-image" className="tvImage"/>
+                    <img src={bestSellingTv.sourceImg} alt="tv-image" className="tv-image"/>
                     <div className="best-product-text-area">
                         <h3>{showProductName(bestSellingTv)}</h3>
                         <h4>{showProductPrice(bestSellingTv)}</h4>
@@ -71,6 +80,35 @@ function App() {
                     <button type={"button"} onClick={cheapestFirst}>Goedkoopste eerst</button>
                     <button type={"button"} onClick={mostSuitedForSport}>Meest geschikt voor sport eerst</button>
                 </div>
+
+
+
+
+                {inventory.map((productInfo) => {
+                    return <div className="product-panel" key={productInfo.type}>
+                        <span className="tv-image">
+                        <img src={productInfo.sourceImg} alt="tv-image" className="tv-image"/></span>
+                        <div className="best-product-text-area">
+                            <h3>{showProductName(productInfo)}</h3>
+                            <h4>{showProductPrice(productInfo)}</h4>
+                            <h5>{showScreenSizes(productInfo.availableSizes)}</h5>
+                            <ul className="options">
+                                {productInfo.options.map((option) => {
+
+                                    if (option.applicable === true) {
+                                        return <li key={productInfo.type + option.name}><img src={check} alt="check-icon" className="icons"/>{option.name}</li>
+                                    } else {
+                                        return <li key={productInfo.type + option.name}><img src={minus} alt="minus-icon" className="icons"/>{option.name}</li>
+                                    }
+                                })}
+                            </ul>
+                        </div>
+                    </div>
+
+
+                })}
+
+
             </section>
         </main>
     )
